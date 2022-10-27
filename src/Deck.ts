a99d5828aa22291f6333efa2239f0855a9c64eec
Tom Szpytman <mail@tomszpytman.com>
2022-10-27 16:05:18 +0100

    chore: add concrete Deck implementation

---
+++ src/Deck.ts
@@ -0,0 +1,31 @@
+import { ICard, IRank, ISuit } from './interfaces'
+import { Card } from './Card'
+
+export const NUM_HIDDEN_CARDS = 3
+export const NUM_USER_ROUND_CARDS = 7
+
+export class Deck {
+  private cards: ICard[]
+
+  public constructor() {
+    this.build()
+    this.shuffle()
+  }
+
+  private build() {
+    this.cards = Object.values(ISuit).map((suit) => {
+      return Object.values(IRank).map((rank) => new Card(rank, suit))
+    }).reduce((acc, curr) => ([...acc, ...curr]), [] as ICard[])
+  }
+
+  private shuffle() {
+    this.cards = this.cards
+      .map(value => ({value, sort: Math.random()}))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({value}) => value)
+  }
+
+  public getNCards(n: number) {
+    return this.cards.splice(0, n)
+  }
+}
