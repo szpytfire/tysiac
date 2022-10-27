9e09420fc309a9ad5ae6d17fed3bb72aeea06002
Tom Szpytman <mail@tomszpytman.com>
2022-10-27 16:05:04 +0100

    chore: add concrete Card implementation

---
+++ src/Card.ts
@@ -0,0 +1,20 @@
+import { ICard, IRank, ISuit } from './interfaces'
+
+export class Card implements ICard {
+  value: number
+
+  public constructor(public rank: IRank, public suit: ISuit) {
+    this.value = this.calculateCardValue()
+  }
+
+  private calculateCardValue (): number {
+    switch (this.rank) {
+      case 'NINE': return 0
+      case 'JACK': return 2
+      case 'ACE': return 11
+      case 'TEN': return 10
+      case 'QUEEN': return 4
+      case 'KING': return 5
+    }
+  }
+}
